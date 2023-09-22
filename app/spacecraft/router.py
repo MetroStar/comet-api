01e2b44004976784487b0b6cea3ebc8c92fb15c8
Johnny Bouder <jbouder@metrostar.com>
2023-09-22 08:26:00 -0400

    Update endpoint names.

--- app/spacecraft/router.py
+++ app/spacecraft/router.py
@@ -11,14 +11,14 @@ router = APIRouter(
 )
 
 @router.get('/spacecraft', response_model=List[Spacecraft])
-def get_characters_view(db: Session = Depends(get_db)):
+def get_all_spacecraft(db: Session = Depends(get_db)):
     return get_spacecraft(db)
 
 @router.get('/spacecraft/{spacecraft_id}')
-def get_characters_view(spacecraft_id: int, db: Session = Depends(get_db)):
+def get_spacecraft(spacecraft_id: int, db: Session = Depends(get_db)):
     return get_spacecraft(db, spacecraft_id)
 
 @router.post('/spacecraft/', response_model=Spacecraft)
-def create_characters_view(spacecraft: Spacecraft, db: Session = Depends(get_db)):
+def create_spacecraft(spacecraft: Spacecraft, db: Session = Depends(get_db)):
     db_spacecraft = create_spacecraft(db, spacecraft)
     return db_spacecraft
