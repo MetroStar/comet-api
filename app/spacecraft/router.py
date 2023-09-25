6782c02ed1caa99850969cc947f43ef2c631946d
Johnny Bouder <jbouder@metrostar.com>
2023-09-25 09:36:32 -0400

    Update db session variable.

--- app/spacecraft/router.py
+++ app/spacecraft/router.py
@@ -11,18 +11,18 @@ router = APIRouter(
     responses={404: {"description": "Endpoint not found"}},
 )
 
-# Database dependency injection context
-db_context = Annotated[Session, Depends(get_db)]
+# Database dependency injection session
+db_session = Annotated[Session, Depends(get_db)]
 
 @router.get('/spacecraft', response_model=List[Spacecraft])
-def get_all_spacecraft(db: db_context):
+def get_all_spacecraft(db: db_session):
     return model.get_all_spacecraft(db)
 
 @router.get('/spacecraft/{id}')
-def get_spacecraft(id: int, db: db_context):
+def get_spacecraft(id: int, db: db_session):
     return model.get_spacecraft(db, id)
 
 @router.post('/spacecraft/', response_model=Spacecraft)
-def create_spacecraft(spacecraft: Spacecraft, db: db_context):
+def create_spacecraft(spacecraft: Spacecraft, db: db_session):
     db_spacecraft = model.create_spacecraft(db, spacecraft)
     return db_spacecraft
