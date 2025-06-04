ecf7284ab5a5179845f49373b3b5ef96d42b65dd
Johnny Bouder <jbouder@metrostar.com>
2025-06-04 14:27:37 -0400

    Update settings config to resolve deprecation warning.

--- app/config.py
+++ app/config.py
@@ -1,12 +1,11 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env")
+
     DATABASE_URL: str
     OIDC_CONFIG_URL: str
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
