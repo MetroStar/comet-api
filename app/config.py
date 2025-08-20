d5c76b7efdfdafbfee81ecaaa97e206937eb28d6
Johnny Bouder <jbouder@metrostar.com>
2025-08-20 08:42:45 -0400

    Remove none type from database url as setting is required.

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
-    DATABASE_URL: str | None = "sqlite:///./db.sqlite3"
+    DATABASE_URL: str = "sqlite:///./db.sqlite3"
     OIDC_CONFIG_URL: str | None = None
 
 
